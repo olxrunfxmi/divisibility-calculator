9f6a19963384238e16a6c3137cd71c6133443df3
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-06-08 22:00:54 +0100

    Add multiplication table

---
+++ index.js
@@ -0,0 +1,42 @@
+const tableEl = document.querySelector(".table");
+const tableButtonEl = document.querySelector("#table-button");
+
+createMultiplyTable(10);
+
+tableButtonEl.addEventListener("click", (e) => {
+	tableEl.dataset.open = tableEl.dataset.open === "true" ? "false" : "true";
+});
+
+function createMultiplyTable(num) {
+	const startNum = 1;
+	const endNum = num;
+
+	for (let index = startNum; index <= endNum; index++) {
+		const startEl = createElement("div", index, ["table-item"]);
+		tableEl.appendChild(startEl);
+
+		for (let innerIndex = 1; innerIndex <= endNum; innerIndex++) {
+			const numberEl = createElement(
+				"div",
+				index * innerIndex,
+				["table-item"],
+				{
+					name: "info",
+					value: `${index} x ${innerIndex} = ${index * innerIndex}`,
+				}
+			);
+			tableEl.appendChild(numberEl);
+		}
+	}
+}
+
+function createElement(elementType, textContent, classArr, data) {
+	const element = document.createElement(elementType);
+	element.textContent = textContent;
+	element.classList.add(...classArr);
+	if (data !== undefined) {
+		element.dataset[data.name] = data.value;
+	}
+
+	return element;
+}
